aaebaa5d65ab6cd0f33577df69eae57c34aa2df0
Geert Audenaert <geert@rogerthat.net>
2018-10-16 11:00:09 +0200

    Allow chaining (#1855)

--- apps/cloudbroker/cloudbrokerlib/objectqueue.py
+++ apps/cloudbroker/cloudbrokerlib/objectqueue.py
@@ -1,3 +1,6 @@
+import inspect
+from collections import namedtuple
+
 from gevent import spawn, spawn_later
 from gevent.queue import Queue
 
@@ -8,10 +11,16 @@ class ObjectQueue(object):
 
     Eg queue a method for a vm object with id 4, retry 3 times before failing, and do not block
     oq = ObjectQueue() # Can also use the ObjectQueue.get_instance() method.
-    oq.queue("vm", 4, 3, False, my_method, my_method_arg, my_method_kwarg="moehaha")
+    oq.queue("vm", 4, 3, my_method, my_method_arg, my_method_kwarg="moehaha")
 
     Eg queue a method for a vm object with id 4, retry 3 times before failing, and block to get the result
-    my_method_result =  oq.queue("vm", 4, 3, True, my_method, my_method_arg, my_method_kwarg="moehaha")
+    future = oq.queue("vm", 4, 3, my_method, my_method_arg, my_method_kwarg="moehaha")
+    future.get_result() # This method raises if an exception would be thrown in my_method
+
+    Eg queue a method for a vm object with id 4, retry 3 times before failing, and chain another method, then get the final result
+    future = oq.queue("vm", 4, 3, my_method, my_method_arg, my_method_kwarg="moehaha")
+    future2 = future.chain("vm", 4, 3, my_other_method, my_method_arg, my_method_kwarg="moehaha")
+    future2.get_result() # This method raises if an exception would be thrown in my_method or my_other_method
 
     Eg stop the queue for a specific object, because it was deleted
     oq.stop("vm", 4)
@@ -45,49 +54,90 @@ class ObjectQueue(object):
             spawn_later(3600, cleanup)
         spawn_later(3600, cleanup)
 
-    def queue(
-        self,
-        object_type,
-        object_id,
-        retry_count,
-        wait_for_result,
-        method,
-        *args,
-        **kwargs
-    ):
-        if self.terminating:
-            raise RuntimeError(
-                "The object queue is terminating. No additional requests can be queued"
-            )
-        self._validate_object_type(object_type)
-        queue_name = self._get_queue_name(object_type, object_id)
-        if not queue_name in self.queues:
+    class _Future(object):
 
-            def runner(queue):
-                for result_queue, retry_count, method, args, kwargs in queue:
+        _Result = namedtuple('Result', ['success', 'value'])
+        _Details = namedtuple('FutureDetails', ['parent_future', 'object_type', 'object_id', 'method', 'args', 'kwargs'])
+
+        def __init__(self, parent_future, object_type, object_id, retry_count, method, *args, **kwargs):
+            if not isinstance(retry_count, int):
+                raise ValueError("retry_count argument should be an integer")
+            if retry_count <= 0:
+                raise ValueError("retry_count can not be less or equal to zero")
+            if not callable(method):
+                raise ValueError("method should be a method or function reference")
+            self._listeners = list()
+            self._result = None
+            self.details = ObjectQueue._Future._Details(parent_future, object_type, object_id, method, args, kwargs)
+            def run():
+                def shackle():
+                    def notify():
+                        for listener in self._listeners:
+                            listener.put("ping")
+                    if self.details.parent_future:
+                        result = self.details.parent_future.get_result_tuple()
+                        if not result.success:
+                            self._result = result
+                            notify()
+                            return
                     for _ in xrange(retry_count):
                         try:
-                            result = method(*args, **kwargs)
-                            if result_queue:
-                                result_queue.put((True, result))
+                            _, _, varkw, _ = inspect.getargspec(method)
+                            if varkw:
+                                kwargs['_future_context'] = self
+                            self._result = ObjectQueue._Future._Result(True, method(*args, **kwargs))
                             break
                         except BaseException as e:
                             # Need to file ECO here
                             pass
                     else:
-                        if result_queue:
-                            result_queue.put((False, e))
+                        self._result = ObjectQueue._Future._Result(False, e)
+                    notify()
+                ObjectQueue.get_instance()._queue(object_type, object_id, shackle)
+            run()
 
+        def chain(self, object_type, object_id, retry_count, method, *args, **kwargs):
+            return ObjectQueue._Future(self, object_type, object_id, retry_count, method, *args, **kwargs)
+
+        def get_result_tuple(self):
+            if self._result:
+                return self._result
+            listener = Queue()
+            self._listeners.append(listener)
+            listener.get()
+            return self.get_result_tuple()
+
+        def get_result(self):
+            result = self.get_result_tuple()
+            if result.success:
+                return result.value
+            else:
+                raise result.value
+
+    def queue(self, object_type, object_id, retry_count, method, *args, **kwargs):
+        future = ObjectQueue._Future(None, object_type, object_id, retry_count, method, *args, **kwargs)
+        return future
+
+    def _queue(self, object_type, object_id, shackle):
+        if self.terminating:
+            raise RuntimeError(
+                "The object queue is terminating. No additional requests can be queued"
+            )
+        queue = self._get_queue(object_type, object_id)
+        queue.put(shackle)
+    
+    def _get_queue(self, object_type, object_id):
+        self._validate_object_type(object_type)
+        queue_name = self._get_queue_name(object_type, object_id)
+        if not queue_name in self.queues:
+            def runner(queue):
+                for shackle in queue:
+                    shackle()
             queue = Queue()
             self.queues[queue_name] = queue
             spawn(runner, queue)
-        result_queue = Queue() if wait_for_result else None
-        self.queues[queue_name].put((result_queue, retry_count, method, args, kwargs))
-        if wait_for_result:
-            success, result = result_queue.get()
-            if success:
-                return result
-            raise result
+            return queue
+        return self.queues[queue_name]
 
     def stop(self, object_type, object_id):
         self._validate_object_type(object_type)
