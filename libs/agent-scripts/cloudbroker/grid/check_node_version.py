da7fef21c6a9119e6795f69dfcd8045602f0d0dc
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-10-17 08:39:17 -0700

    update check_node_version script (#1911)

--- libs/agent-scripts/cloudbroker/grid/check_node_version.py
+++ libs/agent-scripts/cloudbroker/grid/check_node_version.py
@@ -15,47 +15,27 @@ queue = "process"
 log = True
 
 
-def get_current_version():
-    repos = dict()
-    version = dict()
-    repos.update(j.do.getGitReposListLocal("github", "jumpscale7"))
-    repos.update(j.do.getGitReposListLocal("github", "0-complexity"))
-    for path in repos.values():
-        git = j.clients.git.get(path)
-        slug = "{}/{}".format(git.account, git.name)
-        version[slug] = dict([git.getBranchOrTag()])
-    return version
-
-
-def is_up_to_date(current, target):
-    for repo in target["repos"]:
-        slug = "/".join(repo["url"].split("/")[-2:])
-        if slug not in current.keys():
-            continue
-        if current[slug] != repo["target"]:
-            return False
-    return True
-
-
 def action():
+    nid = j.application.whoAmI.nid
+    gid = j.application.whoAmI.gid
+
     if j.system.platformtype.isVirtual():
         return
+
     scl = j.clients.osis.getNamespace("system")
-    status = scl.node.get(j.application.whoAmI.nid).status
-    if not scl.version.count({"status": "INSTALLING"}) and status == "ENABLED":
-        current = get_current_version()
-        version = scl.version.searchOne({"status": "CURRENT"})
-        target = yaml.load(version["manifest"])
-        if not is_up_to_date(current, target):
-            whoami = j.application.whoAmI
-            nodename = scl.node.get(whoami.nid).name.split(".")[0]
+    node = scl.node.get(nid)
+    if not scl.version.count({"status": "INSTALLING"}) and node.status == "ENABLED":
+        current_version = j.application.config["system"]["version"]
+        version = scl.version.searchOne({"status": "CURRENT"})["name"]
+        if current_version != version:
+            nodename = node.name.split(".")[0]
             args = {"nodename": nodename}
             acl = j.clients.agentcontroller.get()
             acl.executeJumpscript(
                 "greenitglobe",
                 "update_node",
                 role="controller",
-                gid=whoami.gid,
+                gid=gid,
                 args=args,
                 wait=False,
             )
